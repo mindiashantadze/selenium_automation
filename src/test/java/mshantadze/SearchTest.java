3dbda43f885bb78cae121ceace43f86a9ccee6a6
Mindia Shantadze <74853926+MindiaShantadze02@users.noreply.github.com>
2023-04-06 18:12:10 +0400

    added finally blocks to test methods

--- src/test/java/mshantadze/SearchTest.java
+++ src/test/java/mshantadze/SearchTest.java
@@ -21,133 +21,154 @@ public class SearchTest extends BaseTest {
     @Test
     public void searchTest() {
         WebDriver driver = new ChromeDriver();
-        driver.get(configInstance.get("url"));
-        HomePage homePage = new HomePage(driver);
-        homePage.verifyThatPageIsLoaded();
-        homePage.getSearchSection().typeInSearchField("Ball");
-        homePage.getSearchSection().clickSearchButton();
-        ProductListingPage plp = new ProductListingPage(driver);
-        List<WebElement> productNameLbls = plp.getProductNames();
-        for (WebElement productNameLbl : productNameLbls) {
-            String productName = productNameLbl.getText().toLowerCase();
-            LOGGER.info(productName);
+        try {
+            driver.get(configInstance.get("url"));
+            HomePage homePage = new HomePage(driver);
+            homePage.verifyThatPageIsLoaded();
+            homePage.getSearchSection().typeInSearchField("Ball");
+            homePage.getSearchSection().clickSearchButton();
+            ProductListingPage plp = new ProductListingPage(driver);
+            List<WebElement> productNameLbls = plp.getProductNames();
+            for (WebElement productNameLbl : productNameLbls) {
+                String productName = productNameLbl.getText().toLowerCase();
+                LOGGER.info(productName);
 
-            Assert.assertTrue(
-                    productName.contains("ball"),
-                    "Expected element to contain word 'ball' but result was" + productName
-            );
+                Assert.assertTrue(
+                        productName.contains("ball"),
+                        "Expected element to contain word 'ball' but result was" + productName
+                );
+            }
+        } finally {
+            driver.quit();
         }
-        driver.quit();
     }
 
     @Test
     public void emptySearch() {
         WebDriver driver = new ChromeDriver();
-        driver.get(configInstance.get("url"));
-        HomePage homePage = new HomePage(driver);
-        homePage.verifyThatPageIsLoaded();
-        homePage.getSearchSection().clickSearchButton();
-        CategoriesPage categoriesPage = new CategoriesPage(driver);
-        String categoryTitle = categoriesPage.getCategoriesTitle().getText().trim();
-        Assert.assertEquals(categoryTitle, "All Categories", "Element should have text \"All Categories\"");
-        Assert.assertTrue(driver.getCurrentUrl().contains("all-categories"), "url should contain all-categories");
-        driver.quit();
+        try {
+            driver.get(configInstance.get("url"));
+            HomePage homePage = new HomePage(driver);
+            homePage.verifyThatPageIsLoaded();
+            homePage.getSearchSection().clickSearchButton();
+            CategoriesPage categoriesPage = new CategoriesPage(driver);
+            String categoryTitle = categoriesPage.getCategoriesTitle().getText().trim();
+            Assert.assertEquals(categoryTitle, "All Categories", "Element should have text \"All Categories\"");
+            Assert.assertTrue(driver.getCurrentUrl().contains("all-categories"), "url should contain all-categories");
+        } finally {
+            driver.quit();
+        }
     }
 
     @Test
     public void noProductsFound() {
         WebDriver driver = new ChromeDriver();
-        driver.get(configInstance.get("url"));
-        HomePage homePage = new HomePage(driver);
-        homePage.verifyThatPageIsLoaded();
-        homePage.getSearchSection().typeInSearchField("somenonexistingproduct");
-        homePage.getSearchSection().clickSearchButton();
-        ProductListingPage plp = new ProductListingPage(driver);
-        String productNotFoundMessage = plp.getNoProductFoundLbl().getText().trim();
-        Assert.assertEquals(productNotFoundMessage, "No exact matches found", "Message should say that no matches were found");
-        driver.quit();
+        try {
+            driver.get(configInstance.get("url"));
+            HomePage homePage = new HomePage(driver);
+            homePage.verifyThatPageIsLoaded();
+            homePage.getSearchSection().typeInSearchField("somenonexistingproduct");
+            homePage.getSearchSection().clickSearchButton();
+            ProductListingPage plp = new ProductListingPage(driver);
+            String productNotFoundMessage = plp.getNoProductFoundLbl().getText().trim();
+            Assert.assertEquals(productNotFoundMessage, "No exact matches found", "Message should say that no matches were found");
+        } finally {
+            driver.quit();
+        }
     }
 
     @Test
     public void searchWithCategories() {
         WebDriver driver = new ChromeDriver();
-        driver.get(configInstance.get("url"));
-        HomePage homePage = new HomePage(driver);
-        homePage.verifyThatPageIsLoaded();
-        homePage.getSearchSection().typeInSearchField("Ball");
-        homePage.getSearchSection().selectCategory();
-        homePage.getSearchSection().clickSearchButton();
-        ProductListingPage plp = new ProductListingPage(driver);
-        String activeCategoryText = plp.getActiveCategory().getText();
-        Assert.assertEquals(activeCategoryText, "Selected category", "Category should be selected");
-        driver.quit();
+        try {
+            driver.get(configInstance.get("url"));
+            HomePage homePage = new HomePage(driver);
+            homePage.verifyThatPageIsLoaded();
+            homePage.getSearchSection().typeInSearchField("Ball");
+            homePage.getSearchSection().selectCategory();
+            homePage.getSearchSection().clickSearchButton();
+            ProductListingPage plp = new ProductListingPage(driver);
+            String activeCategoryText = plp.getActiveCategory().getText();
+            Assert.assertEquals(activeCategoryText, "Selected category", "Category should be selected");
+            driver.quit();
+        } finally {
+            driver.quit();
+        }
     }
 
     @Test
     public void excludeProducts() {
         WebDriver driver = new ChromeDriver();
-        driver.get(configInstance.get("url"));
-        HomePage homePage = new HomePage(driver);
-        homePage.verifyThatPageIsLoaded();
-        AdvancedSearchPage advancedSearchPage = homePage.getSearchSection().goToAdvancedSearchPage();
-        advancedSearchPage.typeIncludedWords("Ball");
-        advancedSearchPage.typeExcludedWords("Disco");
-        advancedSearchPage.submitFilter();
-        ProductListingPage plp = new ProductListingPage(driver);
-        List<WebElement> productNameLbls = plp.getProductNames();
-        for (WebElement productNameLbl : productNameLbls) {
-            String productName = productNameLbl.getText().toLowerCase();
-            LOGGER.info(productName);
+        try {
+            driver.get(configInstance.get("url"));
+            HomePage homePage = new HomePage(driver);
+            homePage.verifyThatPageIsLoaded();
+            AdvancedSearchPage advancedSearchPage = homePage.getSearchSection().goToAdvancedSearchPage();
+            advancedSearchPage.typeIncludedWords("Ball");
+            advancedSearchPage.typeExcludedWords("Disco");
+            advancedSearchPage.submitFilter();
+            ProductListingPage plp = new ProductListingPage(driver);
+            List<WebElement> productNameLbls = plp.getProductNames();
+            for (WebElement productNameLbl : productNameLbls) {
+                String productName = productNameLbl.getText().toLowerCase();
+                LOGGER.info(productName);
 
-            Assert.assertTrue(
-                    productName.contains("ball") && !productName.contains("disco"),
-                    "Expected element to contain word 'ball' and not 'disco' but result was" + productName
-            );
+                Assert.assertTrue(
+                        productName.contains("ball") && !productName.contains("disco"),
+                        "Expected element to contain word 'ball' and not 'disco' but result was" + productName
+                );
+            }
+        } finally {
+            driver.quit();
         }
 
-        driver.quit();
     }
 
     @Test
     public void includeExactWords() {
         WebDriver driver = new ChromeDriver();
-        driver.get(configInstance.get("url"));
-        HomePage homePage = new HomePage(driver);
-        AdvancedSearchPage advancedSearchPage = homePage.getSearchSection().goToAdvancedSearchPage();
-        advancedSearchPage.typeIncludedWords("Disco Ball");
-        advancedSearchPage.selectExactWordAndOrder();
-        advancedSearchPage.submitFilter();
-        ProductListingPage plp = new ProductListingPage(driver);
-        List<WebElement> productNameLbls = plp.getProductNames();
-        for (WebElement productNameLbl : productNameLbls) {
-            String productName = productNameLbl.getText().toLowerCase();
-            LOGGER.info(productName);
+        try {
+            driver.get(configInstance.get("url"));
+            HomePage homePage = new HomePage(driver);
+            AdvancedSearchPage advancedSearchPage = homePage.getSearchSection().goToAdvancedSearchPage();
+            advancedSearchPage.typeIncludedWords("Disco Ball");
+            advancedSearchPage.selectExactWordAndOrder();
+            advancedSearchPage.submitFilter();
+            ProductListingPage plp = new ProductListingPage(driver);
+            List<WebElement> productNameLbls = plp.getProductNames();
+            for (WebElement productNameLbl : productNameLbls) {
+                String productName = productNameLbl.getText().toLowerCase();
+                LOGGER.info(productName);
 
-            Assert.assertTrue(
-                    productName.contains("disco ball"),
-                    "Expected element to contain word 'disco ball' but result was" + productName
-            );
+                Assert.assertTrue(
+                        productName.contains("disco ball"),
+                        "Expected element to contain word 'disco ball' but result was" + productName
+                );
+            }
+        } finally {
+            driver.quit();
         }
-        driver.quit();
     }
 
     @Test
     public void searchByProductRange() {
         WebDriver driver = new ChromeDriver();
-        driver.get(configInstance.get("url"));
-        HomePage homePage = new HomePage(driver);
-        AdvancedSearchPage advancedSearchPage = homePage.getSearchSection().goToAdvancedSearchPage();
-        advancedSearchPage.typeIncludedWords("Ball");
-        advancedSearchPage.enterMinPrice("20");
-        advancedSearchPage.enterMaxPrice("50");
-        advancedSearchPage.submitFilter();
-        ProductListingPage plp = new ProductListingPage(driver);
-        List<Double> prices = plp.getProductPrices();
-        for (Double price : prices) {
-            LOGGER.info(price.toString());
-            Assert.assertTrue(price > 20 && price < 50, "Price should be more than 20 and less than 50. actual price: " + price);
+        try {
+            driver.get(configInstance.get("url"));
+            HomePage homePage = new HomePage(driver);
+            AdvancedSearchPage advancedSearchPage = homePage.getSearchSection().goToAdvancedSearchPage();
+            advancedSearchPage.typeIncludedWords("Ball");
+            advancedSearchPage.enterMinPrice("20");
+            advancedSearchPage.enterMaxPrice("50");
+            advancedSearchPage.submitFilter();
+            ProductListingPage plp = new ProductListingPage(driver);
+            List<Double> prices = plp.getProductPrices();
+            for (Double price : prices) {
+                LOGGER.info(price.toString());
+                Assert.assertTrue(price > 20 && price < 50, "Price should be more than 20 and less than 50. actual price: " + price);
+            }
+        } finally {
+            driver.quit();
         }
-
-        driver.quit();
     }
 }
--- src/test/java/mshantadze/base/BaseTest.java
+++ src/test/java/mshantadze/base/BaseTest.java
@@ -1,14 +1,24 @@
 package mshantadze.base;
 
 import mshantadze.services.ConfigService;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
+import org.testng.ITestNGMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.xml.XmlTest;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class BaseTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
+    private ConcurrentHashMap<String, WebDriver> drivers = new ConcurrentHashMap<>();
 
     protected ConfigService configInstance;
 
